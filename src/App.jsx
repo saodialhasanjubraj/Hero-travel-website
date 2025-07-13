a157dbd153a0db6f79a478170a2203a451dab07a
saodial hasan <saodialhasan@gmail.com>
2025-07-13 19:59:47 +0600

    new file added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import './App.css'
+
+function App() {
+const router = createBrowserRouter([]) 
+
+  return (
+    <>
+      <RouterProvider router={router}/> 
+    </>
+  )
+}
+
+export default App
